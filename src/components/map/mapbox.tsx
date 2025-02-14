790d17232ca6718b26863efb1424b60e4cfb5137
Jadiger <jturganbaev1999@gmail.com>
2025-02-14 12:41:47 +0500

    Updated

--- src/components/map/mapbox.tsx
+++ src/components/map/mapbox.tsx
@@ -164,7 +164,7 @@ console.log(route);
           background: "white",
           padding: "10px",
           borderRadius: "8px",
-          paddingBottom: "env(safe-area-inset-bottom, 20px)",
+          paddingBottom: "60px",
         }}
       >
         <label>Type of transport:</label>
